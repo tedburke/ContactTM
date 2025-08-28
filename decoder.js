91401fd9a5df48ccc741b7d93828e899b2c13563
tedburke <teddy.burke@gmail.com>
2025-08-28 13:35:35 +0100

    Inserted basic audio capture and visualization code, but haven't tested it yet.

--- decoder.js
+++ decoder.js
@@ -6,12 +6,23 @@ const spectrumCanvas = document.getElementById("spectrumCanvas");
 const startButton = document.getElementById("startButton");
 const pauseButton = document.getElementById("pauseButton");
 
+var drawingCtx, spectrumCtx; // canvas contexts
+
+window.onresize = function() {
+    drawingCanvas.width = drawingCanvas.getBoundingClientRect().width;
+    drawingCanvas.height = drawingCanvas.getBoundingClientRect().height;
+    spectrumCanvas.width = spectrumCanvas.getBoundingClientRect().width;
+    spectrumCanvas.height = spectrumCanvas.getBoundingClientRect().height;
+    drawingCtx = drawingCanvas.getContext('2d', { alpha: false });
+    spectrumCtx = drawingCanvas.getContext('2d', { alpha: false });
+};
+
+window.onresize();
+
 //var N=1024, n;
 //var buffer = [], binary = [], s = [];
 
 var scanningState = 0;    // 1 is scanning, 0 is paused
-
-console.log("Hello from window.onload function");
 
 // Connect buttons to event listeners
 startButton.addEventListener("click", startScanning);
@@ -21,18 +32,73 @@ pauseButton.disabled = true;
 // Error callback function for getUserMedia method
 var errorCallback = function(error) {console.log("Video capture error: ", error.code);};
 
-// For cross-browser compatibility, select getUserMedia method for current browser
-navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+if (navigator.mediaDevices.getUserMedia)
+{
+    console.log("mediaDevices.getUserMedia() is supported.");
+    
+    let onSuccess = function(stream) {
+        console.log("onSuccess function");
+        //const mediaRecorder = new MediaRecorder(stream);
 
-// Open video capture stream
-/*
-    navigator.getUserMedia({video:{mandatory:{minWidth:vidw,maxWidth:vidw,minHeight:vidh,maxHeight:vidh}}}, function(stream)
-        {
-            //vid.src = window.URL.createObjectURL(stream);
-            vid.srcObject=stream;
-            vid.play();
-        }, errorCallback);
+        visualize(stream);
+
+        /*
+        mediaRecorded.ondataavailable = function(e) {
+            console.log("data");
+        }
         */
+    }
+
+    let onError = function(err) {
+        console.log("onError function");
+    }
+
+    navigator.mediaDevices.getUserMedia({audio:true}).then(onSuccess, onError);
+}
+else
+{
+    console.log("mediaDevices.getUserMedia() is not supported.");
+}
+
+function visualize(stream) {
+    // Set up audio context, buffer, and analyser
+    if (!audioCtx) {
+        audioCtx = new AudioContext();
+    }
+    const source = audioCtx.createMediaStreamSource(stream);
+    const bufferLength = 2048;
+    const analyser = audioCtx.createAnalyser();
+    analyser.fftSize = bufferLength;
+    const dataArray = new Uint8Array(bufferLength);
+    source.connect(analyser);
+
+    draw();
+
+    function draw() {
+        const w = spectrumCanvas.width;
+        const h = spectrumCanvas.height;
+
+        // Ask for this function to be called again at next
+        // screen refresh
+        requestAnimationFrame(draw);
+
+        analyser.getByteTimeDomainData(dataArray);
+        
+        spectrumCtx.fillStyle = "rgb(100 100 100)";
+        spectrumCtx.fillRect(0, 0, w, h);
+        spectrumCtx.lineWidth = 1.0;
+        spectrumCtx.strokeStyle = "rgb(0 0 0)";
+        spectrumCtx.beginPath();
+        spectrumCtx.moveTo(0, dataArray[0]);
+        for (let n = 0 ; n < w ; ++n) {
+            spectrumCtx.lineTo(n, dataArray[n]);
+        }
+        spectrumCtx.stroke();
+    }
+}
+
+// For cross-browser compatibility, select getUserMedia method for current browser
+//navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
 
 window.setInterval(draw, 100);
 
@@ -52,7 +118,7 @@ function pauseScanning()
     scanningState = 0;
 }
 
-function draw()
+function olddraw()
 {
     /*
     vidctx.drawImage(vid,0,0);
@@ -62,10 +128,9 @@ function draw()
     */
 
     // Get contexts for both canvases
-    let ctx = drawingCanvas.getContext("2d");
-    let W = drawingCanvas.width;
-    let H = drawingCanvas.height;
-    console.log("drawingCanvas width and height: " + W + "," + H);
+    let ctx = drawingCanvas.getContext('2d', { alpha: false });
+    let W = ctx.canvas.width;
+    let H = ctx.canvas.height;
 
     let dx = 4, dy = 4;
 
@@ -78,14 +143,7 @@ function draw()
     ctx.lineTo(dx, H - dy);
     ctx.lineTo(W - dx, H - dy);
     ctx.lineTo(W - dx, dy);
-    ctx.lineTo(dx, dy);
-    ctx.stroke();
-
-    ctx.beginPath();
-    ctx.strokeStyle = "#BFBFBF";
-    ctx.lineWidth = 1.0;
-    ctx.moveTo(0, 230);
-    ctx.lineTo(W, 230);
+    ctx.closePath();
     ctx.stroke();
 
     ctx.font="20px Arial";
@@ -98,80 +156,22 @@ function draw()
     }
     ctx.stroke();
 
-    ctx = spectrumCanvas.getContext("2d");
-    W = spectrumCanvas.width;
-    H = spectrumCanvas.height;
-    console.log("spectrumCanvas width and height: " + W + "," + H);
+    ctx = spectrumCanvas.getContext('2d', { alpha: false });
+    W = ctx.canvas.width;
+    H = ctx.canvas.height;
 
     // Draw spectrum graph
     ctx.clearRect(0, 0, W, H);
     ctx.strokeStyle = "#ff0000";
-    ctx.lineWidth = 1.0;
-    ctx.beginPath();
-    ctx.moveTo(dx, dy);
-    ctx.lineTo(dx, H - dy);
-    ctx.lineTo(W - dx, H - dy);
-    ctx.lineTo(W - dx, dy);
-    ctx.lineTo(dx, dy);
-    ctx.stroke();
-
-    // Draw grid lines on graph
-    /*
-    graphctx.beginPath();
-    graphctx.strokeStyle = "#3F3F3F";
-    graphctx.lineWidth = 1.0;
-    graphctx.moveTo(ox, oy);
-    graphctx.lineTo(ox+150*dx, oy);
-    for (m=5 ; m<150 ; m+=5)
-    {
-        graphctx.moveTo(ox+m*dx, 0);
-        graphctx.lineTo(ox+m*dx, 220);
-    }
-    graphctx.stroke();
-
-    graphctx.font="20px Arial";
-    graphctx.fillStyle = "#BFBFBF";
-    graphctx.textAlign = "center";
-    for (m=0 ; m<=15 ; ++m)
-    {
-        graphctx.fillText(m, ox + m*10*dx, 240);
-    }
-
-    // Plot samples
-    graphctx.font="20px Arial";
-    graphctx.fillStyle = "#00FFFF";
-    graphctx.textAlign = "right";
-    graphctx.fillText("0", ox-5, oy+7);
-    graphctx.beginPath();
-    graphctx.strokeStyle = "#00FFFF";
-    graphctx.lineWidth = 2.0;
-    graphctx.moveTo(ox, oy - dy * buffer[(n+1)%N]);
-    for (m=1 ; m<N ; ++m)
-    {
-        graphctx.lineTo(ox + m*dx, oy - dy * buffer[(n+1+m)%N]);
-    }
-    graphctx.stroke();
-
-    // Draw bounding rectangle around graph
-    graphctx.strokeStyle = "#BFBFBF";
-    graphctx.lineWidth = 2.0;
-    graphctx.strokeRect(ox, 1, (N-1)*dx, 218);
-
+    ctx.lineWidth = 4.0;
+    ctx.strokeRect(2*dx, 2*dx, W-4*dx, H-4*dx);
+    
     // Check if current buffer matches code
-    graphctx.font="20px Arial";
-    graphctx.fillStyle = "#000000";
-    graphctx.textAlign = "center";
-
-    */
+    ctx.font="20px Arial";
+    ctx.fillStyle = "#000000";
+    ctx.textAlign = "center";
 
     // Display status
-    if (scanningState == 0)
-    {
-        document.getElementById("status").innerHTML = "Paused";
-    }
-    else
-    {
-        document.getElementById("status").innerHTML = "Scanning";
-    }           
+    document.getElementById("status").innerHTML = scanningState ? 'Scanning' : 'Paused';
 }
 
--- notes.md
+++ notes.md
@@ -1,13 +1,6 @@
-Useful article on HTML5 audio/video capture:
+Some useful articles / guides / resources:
 
-- https://web.dev/articles/getusermedia-intro
-
-Wikipedia article on the Goertzel algorithm:
-
-- https://en.wikipedia.org/wiki/Goertzel_algorithm
-
-Good article on "Using the MediaStream Recording API":
-
-- https://developer.mozilla.org/en-US/docs/Web/API/MediaStream_Recording_API/Using_the_MediaStream_Recording_API
-
+- [Useful article on HTML5 audio/video capture](https://web.dev/articles/getusermedia-intro)
+- [Wikipedia article on the Goertzel algorithm](https://en.wikipedia.org/wiki/Goertzel_algorithm)
+- [Good article on "Using the MediaStream Recording API"](https://developer.mozilla.org/en-US/docs/Web/API/MediaStream_Recording_API/Using_the_MediaStream_Recording_API)
 
